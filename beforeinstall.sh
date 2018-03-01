b3d6f4323ce7b8ec3b7b081bd3ddc342611244a7
frodo <dasun@duosoftware.com>
2018-03-02 02:00:48 +0530

    Update beforeinstall.sh

--- beforeinstall.sh
+++ beforeinstall.sh
@@ -3,7 +3,7 @@
 
 DIRECTORY=/var/www/html/oldversions
 if [ -d "$DIRECTORY" ]; then
-  mv /var/www/html/* /var/www/html/oldversions/
+  mv /var/www/html/index.php /var/www/html/oldversions/
   else
   mkdir -p /var/www/html/oldversions
   mv /var/www/html/index.php /var/www/html/oldversions/
